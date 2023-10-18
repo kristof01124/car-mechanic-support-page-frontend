bd03d88a866640aef07ccd327e941e1433710099
kristof01124 <kristof01124@mailbox.unideb.hu>
2023-10-18 16:47:48 +0200

    Created placeholder controller class

--- app/src/Controller.tsx
+++ app/src/Controller.tsx
@@ -1,6 +1,19 @@
 
 import { MyDropdownButton, MyDropdownButtonElementInterface } from "./components/MyDropdownButton"
 
+export interface CarData {
+    brand: string,
+    type: string,
+    licensePlate: string,
+    serialNumber: string
+}
+
+export interface OrderData {
+    severity: string,
+    position: string,
+    description: string,
+}
+
 export function getValuesForDropdownMenu(id: string, onClicked: (id: string) => any): MyDropdownButtonElementInterface[] {
     return [
         {
