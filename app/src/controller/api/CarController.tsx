4b1c6294aac9a4cc38905312f74251580fbbc561
LHKadosa <lelekkadosa@gmail.com>
2023-11-24 15:52:51 +0100

    Car and feedback controller

--- app/src/controller/api/CarController.tsx
+++ app/src/controller/api/CarController.tsx
@@ -1,4 +1,5 @@
 import { GetOrderDTO } from "./OrderController";
+import { deleteBody, getBody, patchBody, postBody, url } from "./constants";
 
 export interface CreateCarDTO {
     brand: string,
@@ -10,4 +11,43 @@ export interface CreateCarDTO {
 export interface GetCarDTO {
     relatedOrders: GetOrderDTO[]
     car_id: number,
+}
+
+export class CarController {
+    // GET methods
+    static async getCarById(id: string): Promise<GetCarDTO> {
+        return (await fetch(url + "/Cars/" + id, getBody)).json()
+    }
+
+
+    static async getAllCars(): Promise<GetCarDTO[]> {
+        return (await fetch(url + "/Cars", getBody)).json()
+    }
+
+
+    // POST methods
+    static async createNewCar(body: CreateCarDTO) {
+        return (await fetch(url + "/Cars/", {
+            ...postBody,
+            body: JSON.stringify(body)
+        })
+        )
+    }
+
+    // PATCH methods
+    static async modifyCar(id: string, body: CreateCarDTO) {
+        return (await fetch(url + "/Cars/" + id, {
+            ...patchBody,
+            body: JSON.stringify(body)
+        })
+        )
+    }
+
+    // DELETE methods
+    static async deleteCarById(id: string) {
+        return (await fetch(url + "/Cars/" + id, {
+            ...deleteBody
+        })
+        )
+    }
 }
--- app/src/controller/api/FeedbackController.tsx
+++ app/src/controller/api/FeedbackController.tsx
@@ -1,11 +1,52 @@
+import { deleteBody, getBody, patchBody, postBody, url } from "./constants"
+
 export interface CreateFeedbackDTO {
-    title: string,
-    start_time: string,
-    end_time: string,
-    comment: string,
-    is_successful: boolean
+    title?: string,
+    start_time?: string,
+    end_time?: string,
+    comment?: string,
+    is_successful?: boolean
 }
 
 export interface GetFeedbackDTO extends CreateFeedbackDTO {
-    feedback_id: number
+    feedback_id?: number
+}
+
+export class FeedbackController {
+    // GET methods
+    static async getFeedbackById(id: string): Promise<GetFeedbackDTO> {
+        return (await fetch(url + "/Feedbacks/" + id, getBody)).json()
+    }
+
+
+    static async getAllFeedbacks(): Promise<GetFeedbackDTO[]> {
+        return (await fetch(url + "/Feedbacks", getBody)).json()
+    }
+
+
+    // POST methods
+    static async createNewFeedback(body: CreateFeedbackDTO) {
+        return (await fetch(url + "/Feedbacks/", {
+            ...postBody,
+            body: JSON.stringify(body)
+        })
+        )
+    }
+
+    // PATCH methods
+    static async modifyFeedback(id: string, body: CreateFeedbackDTO) {
+        return (await fetch(url + "/Feedbacks/" + id, {
+            ...patchBody,
+            body: JSON.stringify(body)
+        })
+        )
+    }
+
+    // DELETE methods
+    static async deleteFeedbackById(id: string) {
+        return (await fetch(url + "/Feedbacks/" + id, {
+            ...deleteBody
+        })
+        )
+    }
 }
