93fd4325aaf2fe68409d0a5ac717dd9697f36646
LHKadosa <lelekkadosa@gmail.com>
2023-11-26 15:33:08 +0100

    Implemented localStorage

--- app/src/controller/session/session.tsx
+++ app/src/controller/session/session.tsx
@@ -1,3 +1,13 @@
 import { GetUserDto } from "../api/UserController";
 
-export var currentUser: GetUserDto;
+export function getCurrentUser() {
+    return JSON.parse(localStorage.getItem("currentUser") ?? "") as GetUserDto
+}
+
+export function clearCurrentUser() {
+    localStorage.setItem("currentUser", "")
+}
+
+export function setCurrentUser(newData: GetUserDto) {
+    localStorage.setItem("currentUser", JSON.stringify(newData))
+}
